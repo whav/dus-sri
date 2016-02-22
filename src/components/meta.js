fd8568c0c92a8f4036d8844e8e2062e29960d11c
Sean Mc Allister <sean@gmx.org>
2016-02-22 13:06:05 +0100

    remove iframe dependency

--- src/components/meta.js
+++ src/components/meta.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import IFrame from 'react-iframe';
 
 class Meta extends Component {
     render() {
-        let items = this.props.items,
-            current_ts = this.props.currentTime;
-
-        items = items.filter((x) => {
-            return (x.start <= current_ts) && (current_ts <= x.end)
-        });
-
-        let m = null;
-        if (items.length > 0) {
-            var src = items[0].links[0];
-            m = <div><IFrame url={src + '#main'} width={"1042px"}/></div>;
-        }
 
         return (<div>
-            {/*{JSON.stringify(this.props)}*/}
-            <hr />
-            {/*{JSON.stringify(items)}*/}
-            {m}
+            {JSON.stringify(this.props)}
         </div>);
     }
 }
